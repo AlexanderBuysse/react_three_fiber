cb341208636c30e80205a92a5b5b88918fb2de85
Alexander Buysse <alexanderbuysse@MacBook-Pro-van-Alexander.local>
2023-03-22 21:34:04 +0100

    Eerste animatie speelt als je op kaart klikt

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,43 @@
 import './App.css'
-import * as THREE from 'three'
-import {createRoot} from 'react-dom/client'
-import React, {useRef, useState} from 'react'
-import {Canvas, useFrame} from '@react-three/fiber'
-import {getProject} from '@theatre/core'
+import React, {useEffect, useState} from 'react'
+import {Canvas} from '@react-three/fiber'
 import studio from '@theatre/studio'
 import extension from '@theatre/r3f/dist/extension'
+import {SheetProvider, editable as e, PerspectiveCamera} from '@theatre/r3f'
+import {getProject} from '@theatre/core'
+import demoProjectState from './state.json'
+import { PlayingCard } from './components/PlayingCard'
+import { OrbitControls } from '@react-three/drei'
 
 studio.initialize()
 studio.extend(extension)
 
+const demoSheet = getProject('Demo Project', {state: demoProjectState}).sheet('Demo Sheet')
 
-// our Theatre.js project sheet, we'll use this later
-const demoSheet = getProject('Demo Project').sheet('Demo Sheet')
 
 const App = () => {
+  const [startAnimation, setStartAnimation] = useState(false);
+
+  useEffect(() => {
+    demoSheet.project.ready.then(() => demoSheet.sequence.play({iterationCount: 1, range: [0, 5]}))
+  }, [startAnimation])
+  
   return (
-    <Canvas camera={{
-      position: [5, 5, -5],
-      fov: 75
-    }}>
-      <ambientLight/>
-      <pointLight position={[10, 10, 10]}/>
-      <mesh onClick={()=> {console.log('dit gebeurt')}}>
-        <boxGeometry args={[1, 1, 1]}/>
-        <meshStandardMaterial color="orange"/>
-      </mesh>
+    <Canvas>
+      <OrbitControls/>
+      <SheetProvider sheet={demoSheet}>
+        <PerspectiveCamera theatreKey="Camera" makeDefault position={[5, 5, -5]} fov={75}/>
+        <ambientLight/>
+        <e.pointLight theatreKey="Light" position={[10, 10, 10]}/>
+        <PlayingCard onClick={() => {
+          console.log('klik op de kaart');
+          setStartAnimation(!startAnimation);
+        }} scale={(.01)} position={[1, 1, 1]}/>
+        <e.mesh theatreKey="Cube">
+          <boxGeometry args={[1, 1, 1]}/>
+          <meshStandardMaterial color="orange"/>
+        </e.mesh>
+      </SheetProvider>
     </Canvas>
   )
 }
